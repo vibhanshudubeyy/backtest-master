be9af4c06e880c9c7072bcd00ed2029425210ac9
vibhanshu <vibhanshudeepakdubey@gmail.com>
2025-02-11 11:28:48 +0530

    updated app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { BeginnerGuide } from "./pages/BeginnerGuide";
+import { SignIn } from "./pages/SignIn";
+import { Signup } from "./pages/SignUp";
 import { Home } from "./pages/Home";
 // import data from './data.json';
 
